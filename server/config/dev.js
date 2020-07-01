9f1f8e75425c9ddcf93134a6e7553a69101d051f
Animesh Landge <anni.landge@gmail.com>
2020-07-01 13:41:51 +0530

    Add files via upload

---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : 'mongodb+srv://anni0911:fuehrer007@cluster0-dwsf6.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
+}
