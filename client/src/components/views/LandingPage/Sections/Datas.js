0b0ad4db887b3fcd48b41aa1e9d990c894100a90
animesh0911 <anni.landge@gmail.com>
2020-07-01 13:38:08 +0530

    .

---
+++ client/src/components/views/LandingPage/Sections/Datas.js
@@ -0,0 +1,72 @@
+
+const price = [
+    {
+        "_id": 0,
+        "name": "Any",
+        "array": []
+    },
+    {
+        "_id": 1,
+        "name": "Rs.0 to Rs.199",
+        "array": [0, 199]
+    },
+    {
+        "_id": 2,
+        "name": "Rs.200 to Rs.249",
+        "array": [200, 249]
+    },
+    {
+        "_id": 3,
+        "name": "Rs.250 to Rs.279",
+        "array": [250, 279]
+    },
+    {
+        "_id": 4,
+        "name": "Rs.80 to Rs.299",
+        "array": [280, 299]
+    },
+    {
+        "_id": 5,
+        "name": "More than Rs.300",
+        "array": [300, 1500000]
+    }
+]
+
+
+
+const continents = [
+    {
+        "_id": 1,
+        "name": "Electronics"
+    },
+    {
+        "_id": 2,
+        "name": "Groceries"
+    },
+    {
+        "_id": 3,
+        "name": "Clothing"
+    },
+    {
+        "_id": 4,
+        "name": "Books"
+    },
+    {
+        "_id": 5,
+        "name": "Stationary"
+    },
+    {
+        "_id": 6,
+        "name": "Furniture"
+    },
+    {
+        "_id": 7,
+        "name": "Footwear"
+    }
+] 
+
+
+export {
+    price,
+    continents
+}
