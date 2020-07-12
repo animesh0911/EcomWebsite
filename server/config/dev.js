942659d2592c1f9b107a78a25c459c55173e0826
Animesh Landge <anni.landge@gmail.com>
2020-07-12 19:27:35 +0530

    Delete dev.js

--- server/config/dev.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI : 'mongodb+srv://anni0911:fuehrer007@cluster0-dwsf6.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
-}
